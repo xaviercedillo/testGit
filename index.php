ea4b2a6a0be757ffdd96f82fc2e642b8fc9975ed
Xavier <xavier.cedillo@dit.mx>
2016-04-27 10:05:49 -0500

    Primer commit de archivos

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+ <title>Test de GitHub</title>
+ <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+ </head>
+ <style></style>
+ <body>
+<h1>Hola Mundo</h1>
+
+<h3>Pruebas de uso de GitHub</h3>
+
+
+
+</body>
+</html>
+ 
+ 
+<script>
+</script>
